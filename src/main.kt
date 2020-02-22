bb54a1e0667fb10127409894ba9e93cb60b43ecf
arte921 <ardjanve@gmail.com>
2020-02-22 21:56:00 +0100

    bbp alg.

--- src/main.kt
+++ src/main.kt
@@ -1,65 +1,119 @@
 import java.lang.Math.random
-import java.math.BigInteger
 import kotlin.math.PI
 import kotlin.math.abs
 import kotlin.math.pow
-import kotlin.math.round
 
-val mctotal: Double = 50000000.0
 val en: Double = 5000000000.0
+val mctotal: Double = 50000000.0
+val infmax: Double = 90.0
 val cpimax: Double = 4000.0
-var bpimax: Double = 50000000.0
+val bpimax: Double = 50000000.0
+val bbpmax: Double = 500000.0
 
-var mcpi: Double = 0.0
-var simplee: Double = 0.0
+var circle: Double = 0.0
 var bpi: Double = 0.0
 var bpii: Int = 0
 var dbpi: Double = 10.0
 var gamble: Double = 0.0
 var go: Long = 0
+var infn: Double = 0.0
+var infz: Double = 0.0
 
-var cpi: Double = 0.0
+var bbpk: Double = 0.0
+var bbpz: Double = 0.0
 
-var circle: Double = 0.0
+var currentFactor: Double = 1.0
+var preFactor: Double = 1.0
 
-fun getDivider(n: Double): Double{
-    return if(n/2<cpimax){
-        6+n.pow(2)/getDivider(n+2)
-    }else{
-        1.0
-    }
-}
 
 fun main() {
     go = System.currentTimeMillis()
-    simplee = (1+1/en).pow(en)
-    println("e ≈ $simplee in ${System.currentTimeMillis()-go} milliseconds")
+    println("Simple e ≈ ${simplee()} in ${System.currentTimeMillis() - go} milliseconds.")
 
     go = System.currentTimeMillis()
-    cpi = 3+1/getDivider(3.0)
-    println("recursive π ≈ $cpi in ${System.currentTimeMillis()-go} milliseconds")
+    println("bbp π ≈ ${bbp()} in ${System.currentTimeMillis() - go} milliseconds.")
 
     go = System.currentTimeMillis()
+    println("Recursive π ≈ ${cpi()} in ${System.currentTimeMillis() - go} milliseconds.")
+
+    go = System.currentTimeMillis()
+    println("Infinite π ≈ ${infpi()} in ${System.currentTimeMillis() - go} milliseconds.")
+
+    go = System.currentTimeMillis()
+    println("Monte carlo π ≈ ${mcpi()} in ${System.currentTimeMillis() - go} milliseconds.")
+
+    go = System.currentTimeMillis()
+    println("Bogo π ≈ ${bpi()} in ${System.currentTimeMillis() - go} milliseconds.")
+
+    println("Kotlin π ≈ $PI")
+}
+
+fun bbp(): Double {
+    bbpk = 0.0
+    bbpz = 0.0
+    while (bbpk < bbpmax) {
+        bbpz += 1/16.0.pow(bbpk)*(4/(8*bbpk+1)-2/(8*bbpk+4)-1/(8*bbpk+5)-1/(8*bbpk+6))
+        bbpk++
+    }
+    return bbpz
+}
+
+fun simplee(): Double {
+    return (1 + 1 / en).pow(en)
+}
+
+fun cpi(): Double {
+    return 3 + 1 / getDivider(3.0)
+}
+
+fun infpi(): Double {
+    infn = 0.0
+    infz = 0.0
+    while (infn < infmax) {
+        infz += 2.0.pow(infn)*factorial(infn).pow(2.0)/factorial(2*infn+1)
+        infn++
+    }
+    return infz * 2
+}
+
+fun mcpi(): Double {
+    circle = 0.0
     val points = List(mctotal.toInt()) { Point() }
     points.forEach {
         if (it.inCircle()) {
             circle++
         }
     }
-    mcpi = circle * 4 / mctotal
+    return circle * 4 / mctotal
+}
 
-    println("mote carlo π ≈ $mcpi in ${System.currentTimeMillis()-go} milliseconds")
-
-    go = System.currentTimeMillis()
-    while(bpii<bpimax){
-        gamble = random()*10.0
-
-        if(abs(gamble - PI) < dbpi){
+fun bpi(): Double {
+    bpii = 0
+    while (bpii < bpimax) {
+        gamble = random() * 10.0
+        if (abs(gamble - PI) < dbpi) {
             bpi = gamble
             dbpi = abs(gamble - PI)
         }
         bpii++
     }
+    return bpi
+}
 
-    println("bogo π ≈ $bpi in ${System.currentTimeMillis()-go} milliseconds")
+fun getDivider(n: Double): Double {
+    return if (n / 2 < cpimax) {
+        6 + n.pow(2) / getDivider(n + 2)
+    } else {
+        1.0
+    }
+}
+
+fun factorial(n: Double): Double {
+    currentFactor = 0.0
+    preFactor = 1.0
+    while (currentFactor < n) {
+        currentFactor++
+        preFactor *= currentFactor
+    }
+    return preFactor
 }
