94f44d5a7db3e1f1a58d54bd0087ed2eba6e995f
arte921 <ardjanve@gmail.com>
2020-02-24 17:36:34 +0100

    bbp alg.

--- src/main.kt
+++ src/main.kt
@@ -2,6 +2,7 @@ import java.lang.Math.random
 import kotlin.math.PI
 import kotlin.math.abs
 import kotlin.math.pow
+import kotlin.math.sqrt
 
 val en: Double = 5000000000.0
 val mctotal: Double = 50000000.0
@@ -9,24 +10,30 @@ val infmax: Double = 90.0
 val cpimax: Double = 4000.0
 val bpimax: Double = 50000000.0
 val bbpmax: Double = 500000.0
+val gpimax: Double = 5000.0
 
 var circle: Double = 0.0
 var bpi: Double = 0.0
 var bpii: Int = 0
-var dbpi: Double = 10.0
+var dbpi: Double = 0.0
 var gamble: Double = 0.0
 var go: Long = 0
 var infn: Double = 0.0
 var infz: Double = 0.0
-
+var bfpi: Double = 0.0
 var bbpk: Double = 0.0
 var bbpz: Double = 0.0
+var gpix: Double = 0.0
+var gpiy: Double = 0.0
+var gpic: Double = 0.0
 
 var currentFactor: Double = 1.0
 var preFactor: Double = 1.0
 
 
 fun main() {
+    println("Kotlin π ≈ $PI")
+
     go = System.currentTimeMillis()
     println("Simple e ≈ ${simplee()} in ${System.currentTimeMillis() - go} milliseconds.")
 
@@ -37,7 +44,10 @@ fun main() {
     println("Recursive π ≈ ${cpi()} in ${System.currentTimeMillis() - go} milliseconds.")
 
     go = System.currentTimeMillis()
-    println("Infinite π ≈ ${infpi()} in ${System.currentTimeMillis() - go} milliseconds.")
+    println("Infinite series π ≈ ${infpi()} in ${System.currentTimeMillis() - go} milliseconds.")
+
+    go = System.currentTimeMillis()
+    println("grid π ≈ ${gpi()} in ${System.currentTimeMillis() - go} milliseconds.")
 
     go = System.currentTimeMillis()
     println("Monte carlo π ≈ ${mcpi()} in ${System.currentTimeMillis() - go} milliseconds.")
@@ -45,7 +55,8 @@ fun main() {
     go = System.currentTimeMillis()
     println("Bogo π ≈ ${bpi()} in ${System.currentTimeMillis() - go} milliseconds.")
 
-    println("Kotlin π ≈ $PI")
+    go = System.currentTimeMillis()
+    println("Bruteforce π ≈ ${bfpi()} in ${System.currentTimeMillis() - go} milliseconds.")
 }
 
 fun bbp(): Double {
@@ -64,6 +75,20 @@ fun simplee(): Double {
 
 fun cpi(): Double {
     return 3 + 1 / getDivider(3.0)
+}
+
+fun gpi(): Double {
+    gpix=0.0
+    gpic=0.0
+    while(gpix<gpimax){
+        gpiy=0.0
+        while(gpiy<=gpimax){
+            if(sqrt(gpix.pow(2)+gpiy.pow(2))<=gpimax) gpic++
+            gpiy++
+        }
+        gpix++
+    }
+    return gpic*4/gpimax.pow(2)
 }
 
 fun infpi(): Double {
@@ -88,6 +113,7 @@ fun mcpi(): Double {
 }
 
 fun bpi(): Double {
+    dbpi = 0.0
     bpii = 0
     while (bpii < bpimax) {
         gamble = random() * 10.0
@@ -98,6 +124,14 @@ fun bpi(): Double {
         bpii++
     }
     return bpi
+}
+
+fun bfpi(): Double {
+    bfpi = 10.0
+    while(abs(bfpi - PI)>0.0000001){
+        bfpi = random()*10.0
+    }
+    return bfpi
 }
 
 fun getDivider(n: Double): Double {
